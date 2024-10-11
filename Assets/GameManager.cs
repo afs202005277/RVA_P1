4b75d9ad6af072ee5b1fb198cb8b8daf89748077
Andre Sousa <up202005277@up.pt>
2024-10-11 17:03:16 +0100

    added 2 menus, not visible by default

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -9,6 +9,9 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int currentDifficulty = PlayerPrefs.GetInt("difficulty", 1); // Default to "1" if not set
+        Debug.Log("Game Difficulty: " + currentDifficulty);
+
         GameObject[] allGameObjects = FindObjectsOfType<GameObject>();
 
         foreach (GameObject obj in allGameObjects)
